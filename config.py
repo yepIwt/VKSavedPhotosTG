dc621f37ff34a6fc69407269eae9eaf61ba501a1
Nikita Sergievsky <ReetRay@protonmail.com>
2021-08-24 10:42:26 +0300

    Рефакторинг файла конфигов для поддержки докера

--- config.py
+++ config.py
@@ -1,2 +1,8 @@
-VK_API_KEY = ""
-TELEGRAM_API_KEY = ""
+import os
+
+VK_API_KEY = os.getenv("TELEGRAM_API_TOKEN")
+TELEGRAM_API_KEY = os.getenv("VK_API_TOKEN")
+
+# Для использования без докера
+# VK_API_KEY = ""
+# TELEGRAM_API_KEY = ""
